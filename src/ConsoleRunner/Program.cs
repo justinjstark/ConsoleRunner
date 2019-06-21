da2708780d4ab0bc82e7636fca181058a739df1b
justinjstark <fromgithub@justinjstark.com>
2019-06-21 13:03:23 -0500

    Move scheduler creation to a static factory

--- src/ConsoleRunner/Program.cs
+++ src/ConsoleRunner/Program.cs
@@ -27,20 +27,7 @@ namespace ConsoleRunner
         {
             var serviceProvider = ConfigureServices();
 
-            //Quartz.NET
-            LogProvider.SetCurrentLogProvider(serviceProvider.GetService<ILogProvider>());
-            
-            var quartzProperties = new NameValueCollection
-            {
-                { "quartz.plugin.ShutdownHook.type", "Quartz.Plugin.Management.ShutdownHookPlugin, Quartz.Plugins" },
-                { "quartz.threadPool.threadCount", "8" },
-                { "quartz.jobStore.type", "Quartz.Simpl.RAMJobStore, Quartz" },
-                { "quartz.scheduler.instanceName", "ConsoleRunner" }
-            };
-            var schedulerFactory = new StdSchedulerFactory(quartzProperties);
-            var scheduler = await schedulerFactory.GetScheduler(CancellationToken.None);
-
-            scheduler.JobFactory = serviceProvider.GetRequiredService<IJobFactory>();
+            var scheduler = await SchedulerFactory.GetSchedulerAsync(serviceProvider);
 
             await ScheduleJobsAsync(scheduler, serviceProvider.GetRequiredService<ICronJobsRepository>());
 
